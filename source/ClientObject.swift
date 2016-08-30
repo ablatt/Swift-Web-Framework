34b2b9f3eed097dfd0a18eae5a96ae034a5abb4c
ablatt <ablatt89@gmail.com>
2016-08-30 08:17:54 -0700

    Now handles partial packets being sent. Code should be streamlined a bit in the future.Chunked-encoding also needs to be supported.

--- source/ClientObject.swift
+++ source/ClientObject.swift
@@ -10,9 +10,13 @@ import Foundation
 
 class ClientObject {
     var requestHeader = Dictionary<String, String>();       // dictionary of header values
-    var requestBody:String?                                 // body of the request
     var formData:Dictionary<String, String>?                // form data if POST request
-    var response:String?                             // response to send to client
+    var response:String?                                    // response to send to client
+    
+    // body information
+    var bodyStartingIndex = -1;
+    var requestBody:[String]?                                 // body of the request
+    var bodyLength = 0;
     
     init () {
         self.requestBody = nil;
